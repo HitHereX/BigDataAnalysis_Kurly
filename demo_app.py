5695647af2ab75da32f4bc68cec3bd7107215f6d
HitHere <suhyeonie@gmail.com>
2023-07-13 21:56:51 +0900

    Revert "add : src"
    This reverts commit 849ce76379003033a55567d802f1273dc0e05418.

--- demo_app.py
+++ demo_app.py
@@ -2,14 +2,13 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 from random import randint
-import openai
-
 
 MESSAGES = []
 TOPIC1 = ['적당','싱싱', '신선']
 TOPIC2 = ['알도','포슬포슬','단단']
 TOPIC3 = ['볶음','카레','가루']
 
+<<<<<<< HEAD
 model_engine = "text-davinci-003"
 openai.api_key = "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" #follow step 4 to get a secret_key
 
@@ -49,57 +48,63 @@ def leave_comments(comments = list, keyword = str):
                                 help='다른 고객분들께 여러분의 구매경험을 나누어 주세요', 
                                 height=10)
 
+=======
+>>>>>>> parent of fc7b3c1 (draft : 230713)
 
+def leave_comments(comments = list):
+    message = st.text_area(label="댓글을 달아주세요 ", 
+                            value="내용을 적어주세요.", 
+                            max_chars=100, 
+                            help='input message < 100', 
+                            height=10)
+    
     #등록 버튼 (코멘트가 추가 됨)
     submitted = st.form_submit_button("등록하기")
     if submitted:
         st.balloons()
-        if message != default:
+        if message != "내용을 적어주세요.":
             comments.append(message)   
    
 
 def load_comments(dataframe : pd.DataFrame, to_find : str, num : int) -> list:
     comments = dataframe['리뷰 내용'].tolist()
-    score = dataframe['score'].tolist()
-
-    mapped = []
-    for i in range(len(comments)):
-       mapped.append((comments[i], score[i]))
-    mapped.sort(key = lambda x:x[1], reverse = True)
     include, result = [], []
 
-    for comment in mapped:
-       if to_find in comment[0]: include.append(comment[0])
+    for comment in comments:
+       if to_find in comment: include.append(comment)
     cnt = len(include)
 
     for i in range(num):
        result.append(include[randint(0, len(include))])
-    
 
-    return include[:5], cnt
+    return result, cnt
 
 
 
 def main() :
 
+<<<<<<< HEAD
     st.title("마켓컬리 구매후기 분석 데모 페이지")
     st.write('이 페이지는 제안을 위한 데모 페이지입니다')
+=======
+    st.title("마켓컬리 DEMO")
+>>>>>>> parent of fc7b3c1 (draft : 230713)
 
     #--------------------------------- import
     potato_img = Image.open('Potato.PNG')
     wc_img = Image.open('wc.png')
-    review1 = Image.open('review.png')
-    review2 = Image.open('review2.png')
-    review3 = Image.open('review3.png')
-
     df = pd.read_csv('hehe.csv')
     
 
     col1,col2 = st.columns([2,3])
     # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
+<<<<<<< HEAD
     st.write('  ') #split spaces
     st.write('  ') #split spaces
     st.subheader('구매후기 분석 결과')
+=======
+    st.subheader('댓글 분석 결과')
+>>>>>>> parent of fc7b3c1 (draft : 230713)
     st.write('토픽을 하나만 선택해주세요')
     col3,col4_1, col4_2, col4_3 = st.columns([2,1,1,1])
     # 공간을 2:3 으로 분할하여col3, 4라는 이름을 가진 컬럼을 생성합니다.  
@@ -112,13 +117,10 @@ def main() :
       #감자 상품 정보
       st.header('KF365 감자 1kg')
       st.subheader('4,500원')
-      st.markdown('---')
-      st.markdown('컬리는 국내 농가에서 기른 맛 좋은 감자를 엄선해 문 앞까지\
-                  신선하게 전해드릴게요. 취향에 따라 간단하게 찌거나 구워서 즐겨보세요. 볶음, 튀김 등의 요리로 \
-                  다채롭게 변신시키면 매일 식탁에 올려도 질리지 않을 거예요.    \
-                  [컬리 페이지 링크](https://www.kurly.com/goods/5026448)')
-      st.markdown('---')
-
+      st.write('너 봄 감자가 맛있단다 어쩌구 저쩌구')
+      st.write('우리 감자 맛있어요 많이 사드세요 두번 사드세요')
+      st.write('감자튀김 감자전 감자수제비 해드시고 카레에도 넣어먹고 된장에도 넣어먹고')
+      st.write('배고프다 헤헤 점심은 다들 드셨나요')
 
     # word cloud image
     with col3 : 
@@ -132,7 +134,6 @@ def main() :
           temp = st.checkbox(word)
           if temp :
              selected_keywords.append(word)  
-        st.image(review1)
 
     with col4_2:
        st.subheader('TOPIC 2')
@@ -140,7 +141,6 @@ def main() :
           temp = st.checkbox(word)
           if temp :
              selected_keywords.append(word)
-       st.image(review2)
 
     with col4_3:
        st.subheader('TOPIC 3')
@@ -148,16 +148,12 @@ def main() :
           temp = st.checkbox(word)
           if temp :
              selected_keywords.append(word)
-       st.image(review3)
 
-
-#    print(selected_keywords)
+    print(selected_keywords)
 
     # 데이터 로딩 함수는 여기에!
-    st.write('  ') #split spaces
-    st.write('  ') #split spaces
-
     comments, cnt = load_comments(df, selected_keywords[-1], 5)
+<<<<<<< HEAD
     per = int(cnt*100/(0.2*len(df)))
     if per == 500:
        st.subheader(f'키워드를 선택하시면, 관련 구매후기를 모아보실 수 있어요')
@@ -165,15 +161,16 @@ def main() :
         st.subheader(f'선택하신 "{selected_keywords[-1]}" 을(를) 포함하는 후기는 {cnt:,}개({int(cnt*100/(0.2*len(df)))}%)예요')
 #    print(comments[:5])
 
+=======
+    st.subheader(f'선택하신 키워드 "{selected_keywords[-1]}" 를 포함하는 댓글은 {cnt}개로, {int(cnt*100/len(df))}% 입니다')
+>>>>>>> parent of fc7b3c1 (draft : 230713)
     for comment in comments:
-        temp = comment.split('\n')
-        with st.expander(temp[0]):
-           st.write(comment)
+        st.info(comment)
 
-    st.write('  ') #split spaces
-    st.write('  ') #split spaces
+
+    st.subheader('선택하신 단어를 참고하여 후기를 작성해 주세요')
     with st.form(key='my_form'):
-        leave_comments(MESSAGES, selected_keywords[-1])
+        leave_comments(MESSAGES)
 
 
 if __name__ == "__main__" :
